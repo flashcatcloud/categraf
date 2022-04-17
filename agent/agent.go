54ecb082160b468ccdbb494dc80e12795ee05c28
Ulric Qin <ulric.qin@gmail.com>
2022-04-17 10:50:02 +0800

    add collector: processes

--- agent/agent.go
+++ agent/agent.go
@@ -21,6 +21,7 @@ import (
 	_ "flashcat.cloud/categraf/inputs/mem"
 	_ "flashcat.cloud/categraf/inputs/net"
 	_ "flashcat.cloud/categraf/inputs/netstat"
+	_ "flashcat.cloud/categraf/inputs/processes"
 	_ "flashcat.cloud/categraf/inputs/redis"
 	_ "flashcat.cloud/categraf/inputs/system"
 )
