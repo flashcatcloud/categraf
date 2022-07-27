cd4b278c0e6633285c181fcc75c877c1162bc433
lsy1990 <lsy1990@sina.com>
2022-07-27 15:29:45 +0800

    refactor to remove instance

--- inputs/nfsclient/nfsclient.go
+++ inputs/nfsclient/nfsclient.go
@@ -12,46 +12,12 @@ import (
 	"flashcat.cloud/categraf/config"
 	"flashcat.cloud/categraf/inputs"
 	"flashcat.cloud/categraf/types"
-
-	"github.com/toolkits/pkg/container/list"
 )
 
 const inputName = "nfsclient"
 
 type NfsClient struct {
-	config.Interval
-	Instances []*Instance `toml:"instances"`
-}
-
-func init() {
-	inputs.Add(inputName, func() inputs.Input {
-		return &NfsClient{}
-	})
-}
-
-func (r *NfsClient) Prefix() string {
-	return inputName
-}
-
-func (r *NfsClient) Init() error {
-	return nil
-}
-
-func (r *NfsClient) Drop() {}
-
-func (r *NfsClient) Gather(slist *list.SafeList) {
-
-}
-func (r *NfsClient) GetInstances() []inputs.Instance {
-	ret := make([]inputs.Instance, len(r.Instances))
-	for i := 0; i < len(r.Instances); i++ {
-		ret[i] = r.Instances[i]
-	}
-	return ret
-}
-
-type Instance struct {
-	config.InstanceConfig
+	config.PluginConfig
 
 	Fullstat          bool     `toml:"fullstat"`
 	IncludeMounts     []string `toml:"include_mounts"`
@@ -64,7 +30,17 @@ type Instance struct {
 	mountstatsPath string
 }
 
-func (ins *Instance) Init() error {
+func init() {
+	inputs.Add(inputName, func() inputs.Input {
+		return &NfsClient{}
+	})
+}
+
+func (s *NfsClient) Prefix() string {
+	return inputName
+}
+
+func (s *NfsClient) Init() error {
 	var nfs3Fields = []string{
 		"NULL",
 		"GETATTR",
@@ -164,9 +140,9 @@ func (ins *Instance) Init() error {
 	nfs3Ops := make(map[string]bool)
 	nfs4Ops := make(map[string]bool)
 
-	ins.mountstatsPath = ins.getMountStatsPath()
+	s.mountstatsPath = s.getMountStatsPath()
 
-	if len(ins.IncludeOperations) == 0 {
+	if len(s.IncludeOperations) == 0 {
 		for _, Op := range nfs3Fields {
 			nfs3Ops[Op] = true
 		}
@@ -174,16 +150,16 @@ func (ins *Instance) Init() error {
 			nfs4Ops[Op] = true
 		}
 	} else {
-		for _, Op := range ins.IncludeOperations {
+		for _, Op := range s.IncludeOperations {
 			nfs3Ops[Op] = true
 		}
-		for _, Op := range ins.IncludeOperations {
+		for _, Op := range s.IncludeOperations {
 			nfs4Ops[Op] = true
 		}
 	}
 
-	if len(ins.ExcludeOperations) > 0 {
-		for _, Op := range ins.ExcludeOperations {
+	if len(s.ExcludeOperations) > 0 {
+		for _, Op := range s.ExcludeOperations {
 			if nfs3Ops[Op] {
 				delete(nfs3Ops, Op)
 			}
@@ -193,12 +169,12 @@ func (ins *Instance) Init() error {
 		}
 	}
 
-	ins.nfs3Ops = nfs3Ops
-	ins.nfs4Ops = nfs4Ops
+	s.nfs3Ops = nfs3Ops
+	s.nfs4Ops = nfs4Ops
 
-	if len(ins.IncludeMounts) > 0 {
+	if len(s.IncludeMounts) > 0 {
 		if config.Config.DebugMode {
-			log.Println("D! Including these mount patterns:", ins.IncludeMounts)
+			log.Println("D! Including these mount patterns:", s.IncludeMounts)
 		}
 	} else {
 		log.Println("Including all mounts.")
@@ -207,9 +183,9 @@ func (ins *Instance) Init() error {
 		}
 	}
 
-	if len(ins.ExcludeMounts) > 0 {
+	if len(s.ExcludeMounts) > 0 {
 		if config.Config.DebugMode {
-			log.Println("D! Excluding these mount patterns:", ins.ExcludeMounts)
+			log.Println("D! Excluding these mount patterns:", s.ExcludeMounts)
 		}
 	} else {
 		if config.Config.DebugMode {
@@ -217,9 +193,9 @@ func (ins *Instance) Init() error {
 		}
 	}
 
-	if len(ins.IncludeOperations) > 0 {
+	if len(s.IncludeOperations) > 0 {
 		if config.Config.DebugMode {
-			log.Println("D! Including these operations:", ins.IncludeOperations)
+			log.Println("D! Including these operations:", s.IncludeOperations)
 		}
 	} else {
 		if config.Config.DebugMode {
@@ -227,9 +203,9 @@ func (ins *Instance) Init() error {
 		}
 	}
 
-	if len(ins.ExcludeOperations) > 0 {
+	if len(s.ExcludeOperations) > 0 {
 		if config.Config.DebugMode {
-			log.Println("D! Excluding these mount patterns:", ins.ExcludeOperations)
+			log.Println("D! Excluding these mount patterns:", s.ExcludeOperations)
 		}
 	} else {
 		if config.Config.DebugMode {
@@ -240,8 +216,10 @@ func (ins *Instance) Init() error {
 	return nil
 }
 
-func (ins *Instance) Gather(slist *list.SafeList) {
-	file, err := os.Open(ins.mountstatsPath)
+func (r *NfsClient) Drop() {}
+
+func (s *NfsClient) Gather(slist *types.SampleList) {
+	file, err := os.Open(s.mountstatsPath)
 	if err != nil {
 		log.Println("E! Failed opening the", file, "file:", err)
 		return
@@ -249,14 +227,16 @@ func (ins *Instance) Gather(slist *list.SafeList) {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	if err := ins.processText(scanner, slist); err != nil {
+	if err := s.processText(scanner, slist); err != nil {
 		return
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Println("E!", err)
 	}
-
+}
+func (s *NfsClient) GetInstances() []inputs.Instance {
+	return nil
 }
 
 func convertToUint64(line []string) ([]uint64, error) {
@@ -291,7 +271,7 @@ func convertToUint64(line []string) ([]uint64, error) {
 	return nline, nil
 }
 
-func (ins *Instance) parseStat(mountpoint string, export string, version string, line []string, slist *list.SafeList) error {
+func (s *NfsClient) parseStat(mountpoint string, export string, version string, line []string, slist *types.SampleList) error {
 	tags := map[string]string{"mountpoint": mountpoint, "serverexport": export}
 	nline, err := convertToUint64(line)
 	if err != nil {
@@ -383,41 +363,35 @@ func (ins *Instance) parseStat(mountpoint string, export string, version string,
 
 	switch first {
 	case "READ", "WRITE":
-		fields["ops"] = nline[0]
-		fields["retrans"] = nline[1] - nline[0]
-		fields["bytes"] = nline[3] + nline[4]
-		fields["rtt"] = nline[6]
-		fields["exe"] = nline[7]
-		fields["rtt_per_op"] = 0.0
+		fields["nfsstat_ops"] = nline[0]
+		fields["nfsstat_retrans"] = nline[1] - nline[0]
+		fields["nfsstat_bytes"] = nline[3] + nline[4]
+		fields["nfsstat_rtt"] = nline[6]
+		fields["nfsstat_exe"] = nline[7]
+		fields["nfsstat_rtt_per_op"] = 0.0
 		if nline[0] > 0 {
-			fields["rtt_per_op"] = float64(nline[6]) / float64(nline[0])
+			fields["nfsstat_rtt_per_op"] = float64(nline[6]) / float64(nline[0])
 		}
-		tags["operation"] = first
-		for key, val := range fields {
-			slist.PushFront(types.NewSample("nfsstat_"+key, val, tags, ins.Labels))
-		}
+		tags["nfsstat_operation"] = first
+		slist.PushSamples(inputName, fields, tags)
 	}
 
-	if ins.Fullstat {
+	if s.Fullstat {
 		switch first {
 		case "events":
 			if len(nline) >= len(eventsFields) {
 				for i, t := range eventsFields {
-					fields[t] = nline[i]
+					fields["nfs_events_"+t] = nline[i]
 				}
-				for key, val := range fields {
-					slist.PushFront(types.NewSample("nfs_events_"+key, val, tags, ins.Labels))
-				}
+				slist.PushSamples(inputName, fields, tags)
 			}
 
 		case "bytes":
 			if len(nline) >= len(bytesFields) {
 				for i, t := range bytesFields {
-					fields[t] = nline[i]
+					fields["nfs_bytes_"+t] = nline[i]
 				}
-				for key, val := range fields {
-					slist.PushFront(types.NewSample("nfs_bytes_"+key, val, tags, ins.Labels))
-				}
+				slist.PushSamples(inputName, fields, tags)
 			}
 
 		case "xprt":
@@ -426,33 +400,27 @@ func (ins *Instance) parseStat(mountpoint string, export string, version string,
 				case "tcp":
 					if len(nline)+2 >= len(xprttcpFields) {
 						for i, t := range xprttcpFields {
-							fields[t] = nline[i+2]
+							fields["nfs_xprt_tcp_"+t] = nline[i+2]
 						}
-						for key, val := range fields {
-							slist.PushFront(types.NewSample("nfs_xprt_tcp_"+key, val, tags, ins.Labels))
-						}
+						slist.PushSamples(inputName, fields, tags)
 					}
 				case "udp":
 					if len(nline)+2 >= len(xprtudpFields) {
 						for i, t := range xprtudpFields {
-							fields[t] = nline[i+2]
+							fields["nfs_xprt_udp_"+t] = nline[i+2]
 						}
-						for key, val := range fields {
-							slist.PushFront(types.NewSample("nfs_xprt_udp_"+key, val, tags, ins.Labels))
-						}
+						slist.PushSamples(inputName, fields, tags)
 					}
 				}
 			}
 		}
-		if (version == "3" && ins.nfs3Ops[first]) || (version == "4" && ins.nfs4Ops[first]) {
+		if (version == "3" && s.nfs3Ops[first]) || (version == "4" && s.nfs4Ops[first]) {
 			tags["operation"] = first
 			if len(nline) <= len(nfsopFields) {
 				for i, t := range nline {
-					fields[nfsopFields[i]] = t
+					fields["nfs_ops_"+nfsopFields[i]] = t
 				}
-				for key, val := range fields {
-					slist.PushFront(types.NewSample("nfs_ops_"+key, val, tags, ins.Labels))
-				}
+				slist.PushSamples(inputName, fields, tags)
 			}
 		}
 	}
@@ -460,7 +428,7 @@ func (ins *Instance) parseStat(mountpoint string, export string, version string,
 	return nil
 }
 
-func (ins *Instance) processText(scanner *bufio.Scanner, slist *list.SafeList) error {
+func (s *NfsClient) processText(scanner *bufio.Scanner, slist *types.SampleList) error {
 	var mount string
 	var version string
 	var export string
@@ -489,9 +457,9 @@ func (ins *Instance) processText(scanner *bufio.Scanner, slist *list.SafeList) e
 			continue
 		}
 
-		if len(ins.IncludeMounts) > 0 {
+		if len(s.IncludeMounts) > 0 {
 			skip = true
-			for _, RE := range ins.IncludeMounts {
+			for _, RE := range s.IncludeMounts {
 				matched, _ := regexp.MatchString(RE, mount)
 				if matched {
 					skip = false
@@ -500,8 +468,8 @@ func (ins *Instance) processText(scanner *bufio.Scanner, slist *list.SafeList) e
 			}
 		}
 
-		if !skip && len(ins.ExcludeMounts) > 0 {
-			for _, RE := range ins.ExcludeMounts {
+		if !skip && len(s.ExcludeMounts) > 0 {
+			for _, RE := range s.ExcludeMounts {
 				matched, _ := regexp.MatchString(RE, mount)
 				if matched {
 					skip = true
@@ -511,7 +479,7 @@ func (ins *Instance) processText(scanner *bufio.Scanner, slist *list.SafeList) e
 		}
 
 		if !skip {
-			err := ins.parseStat(mount, export, version, line, slist)
+			err := s.parseStat(mount, export, version, line, slist)
 			if err != nil {
 				return fmt.Errorf("could not parseStat: %w", err)
 			}
@@ -521,7 +489,7 @@ func (ins *Instance) processText(scanner *bufio.Scanner, slist *list.SafeList) e
 	return nil
 }
 
-func (ins *Instance) getMountStatsPath() string {
+func (s *NfsClient) getMountStatsPath() string {
 	path := "/proc/self/mountstats"
 	if os.Getenv("MOUNT_PROC") != "" {
 		path = os.Getenv("MOUNT_PROC")
