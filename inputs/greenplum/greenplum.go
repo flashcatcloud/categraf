121ca3d90316553242c37f3d5d61f147b2794a58
lsy1990 <lsy1990@sina.com>
2022-09-01 08:43:14 +0800

    fix the bug when %7 !=0

--- inputs/greenplum/greenplum.go
+++ inputs/greenplum/greenplum.go
@@ -41,6 +41,7 @@ func (ins *Greenplum) Gather(slist *types.SampleList) {
 	gpstate := strings.Fields(stateValue)
 	if len(gpstate)%7 != 0 {
 		log.Println("E! failed to parse gpstate -m output: %v", gpstate)
+		return
 	}
 	line := len(gpstate) / 7
 	for i := 0; i < line; i++ {
