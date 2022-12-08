5cfaba13919955500acf0807249828db45405459
hubo <hagic.hhj@gmail.com>
2022-12-08 17:27:43 +0800

    Repair the windows ibex agent

--- ibex/task.go
+++ ibex/task.go
@@ -6,12 +6,12 @@ import (
 	"log"
 	"os/exec"
 	"os/user"
-	"path"
+	"path/filepath"
+	"runtime"
 	"strings"
 	"sync"
 
 	"github.com/toolkits/pkg/file"
-	"github.com/toolkits/pkg/runner"
 	"github.com/toolkits/pkg/sys"
 
 	"flashcat.cloud/categraf/config"
@@ -83,16 +83,16 @@ func (t *Task) ResetBuff() {
 }
 
 func (t *Task) doneBefore() bool {
-	doneFlag := path.Join(config.Config.Ibex.MetaDir, fmt.Sprint(t.Id), fmt.Sprintf("%d.done", t.Clock))
+	doneFlag := filepath.Join(config.Config.Ibex.MetaDir, fmt.Sprint(t.Id), fmt.Sprintf("%d.done", t.Clock))
 	return file.IsExist(doneFlag)
 }
 
 func (t *Task) loadResult() {
 	metadir := config.Config.Ibex.MetaDir
 
-	doneFlag := path.Join(metadir, fmt.Sprint(t.Id), fmt.Sprintf("%d.done", t.Clock))
-	stdoutFile := path.Join(metadir, fmt.Sprint(t.Id), "stdout")
-	stderrFile := path.Join(metadir, fmt.Sprint(t.Id), "stderr")
+	doneFlag := filepath.Join(metadir, fmt.Sprint(t.Id), fmt.Sprintf("%d.done", t.Clock))
+	stdoutFile := filepath.Join(metadir, fmt.Sprint(t.Id), "stdout")
+	stderrFile := filepath.Join(metadir, fmt.Sprint(t.Id), "stderr")
 
 	var err error
 
@@ -119,24 +119,24 @@ func (t *Task) prepare() error {
 		return nil
 	}
 
-	IdDir := path.Join(config.Config.Ibex.MetaDir, fmt.Sprint(t.Id))
+	IdDir := filepath.Join(config.Config.Ibex.MetaDir, fmt.Sprint(t.Id))
 	err := file.EnsureDir(IdDir)
 	if err != nil {
 		log.Printf("E! mkdir -p %s fail: %v", IdDir, err)
 		return err
 	}
 
-	writeFlag := path.Join(IdDir, ".write")
+	writeFlag := filepath.Join(IdDir, ".write")
 	if file.IsExist(writeFlag) {
 		// 从磁盘读取
-		argsFile := path.Join(IdDir, "args")
+		argsFile := filepath.Join(IdDir, "args")
 		args, err := file.ReadStringTrim(argsFile)
 		if err != nil {
 			log.Printf("E! read %s fail %v", argsFile, err)
 			return err
 		}
 
-		accountFile := path.Join(IdDir, "account")
+		accountFile := filepath.Join(IdDir, "account")
 		account, err := file.ReadStringTrim(accountFile)
 		if err != nil {
 			log.Printf("E! read %s fail %v", accountFile, err)
@@ -153,27 +153,36 @@ func (t *Task) prepare() error {
 			return err
 		}
 
-		scriptFile := path.Join(IdDir, "script")
-		_, err = file.WriteString(scriptFile, script)
-		if err != nil {
-			log.Printf("E! write script to %s fail: %v", scriptFile, err)
-			return err
+		switch runtime.GOOS {
+		case "windows":
+			scriptFile := filepath.Join(IdDir, "script.bat")
+			_, err = file.WriteString(scriptFile, fmt.Sprintf("@echo off\r\n%s", script))
+			if err != nil {
+				log.Printf("E! write script to %s fail: %v", scriptFile, err)
+				return err
+			}
+		default:
+			scriptFile := filepath.Join(IdDir, "script")
+			_, err = file.WriteString(scriptFile, script)
+			if err != nil {
+				log.Printf("E! write script to %s fail: %v", scriptFile, err)
+				return err
+			}
+			out, err := sys.CmdOutTrim("chmod", "+x", scriptFile)
+			if err != nil {
+				log.Printf("E! chmod +x %s fail %v. output: %s", scriptFile, err, out)
+				return err
+			}
 		}
 
-		out, err := sys.CmdOutTrim("chmod", "+x", scriptFile)
-		if err != nil {
-			log.Printf("E! chmod +x %s fail %v. output: %s", scriptFile, err, out)
-			return err
-		}
-
-		argsFile := path.Join(IdDir, "args")
+		argsFile := filepath.Join(IdDir, "args")
 		_, err = file.WriteString(argsFile, args)
 		if err != nil {
 			log.Printf("E! write args to %s fail: %v", argsFile, err)
 			return err
 		}
 
-		accountFile := path.Join(IdDir, "account")
+		accountFile := filepath.Join(IdDir, "account")
 		_, err = file.WriteString(accountFile, account)
 		if err != nil {
 			log.Printf("E! write account to %s fail: %v", accountFile, err)
@@ -209,9 +218,15 @@ func (t *Task) start() {
 		args = "'" + args + "'"
 	}
 
-	scriptFile := path.Join(config.Config.Ibex.MetaDir, fmt.Sprint(t.Id), "script")
-	if !path.IsAbs(scriptFile) {
-		scriptFile = path.Join(runner.Cwd, scriptFile)
+	scriptFileType := "script"
+	if runtime.GOOS == "windows" {
+		scriptFileType = "script.bat"
+	}
+
+	scriptFile, err := filepath.Abs(filepath.Join(config.Config.Ibex.MetaDir, fmt.Sprint(t.Id), scriptFileType))
+	if err != nil {
+		log.Println("E! cannot get current absolute path:", err)
+		return
 	}
 
 	sh := fmt.Sprintf("%s %s", scriptFile, args)
@@ -223,18 +238,23 @@ func (t *Task) start() {
 		return
 	}
 
-	if loginUser.Username == "root" {
-		// current login user is root
-		if t.Account == "root" {
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("cmd", "/C", sh)
+	default:
+		if loginUser.Username == "root" {
+			// current login user is root
+			if t.Account == "root" {
+				cmd = exec.Command("sh", "-c", sh)
+				cmd.Dir = loginUser.HomeDir
+			} else {
+				cmd = exec.Command("su", "-c", sh, "-", t.Account)
+			}
+		} else {
+			// current login user not root
 			cmd = exec.Command("sh", "-c", sh)
 			cmd.Dir = loginUser.HomeDir
-		} else {
-			cmd = exec.Command("su", "-c", sh, "-", t.Account)
 		}
-	} else {
-		// current login user not root
-		cmd = exec.Command("sh", "-c", sh)
-		cmd.Dir = loginUser.HomeDir
 	}
 
 	cmd.Stdout = &t.Stdout
@@ -282,9 +302,9 @@ func runProcess(t *Task) {
 func persistResult(t *Task) {
 	metadir := config.Config.Ibex.MetaDir
 
-	stdout := path.Join(metadir, fmt.Sprint(t.Id), "stdout")
-	stderr := path.Join(metadir, fmt.Sprint(t.Id), "stderr")
-	doneFlag := path.Join(metadir, fmt.Sprint(t.Id), fmt.Sprintf("%d.done", t.Clock))
+	stdout := filepath.Join(metadir, fmt.Sprint(t.Id), "stdout")
+	stderr := filepath.Join(metadir, fmt.Sprint(t.Id), "stderr")
+	doneFlag := filepath.Join(metadir, fmt.Sprint(t.Id), fmt.Sprintf("%d.done", t.Clock))
 
 	file.WriteString(stdout, t.GetStdout())
 	file.WriteString(stderr, t.GetStderr())
