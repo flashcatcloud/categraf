f33d8135342e80f75a2476c45334d88f674b58ce
kongfei <kongfei605@gmail.com>
2023-01-04 18:55:57 +0800

    add systemd plugin

--- agent/metrics_agent.go
+++ agent/metrics_agent.go
@@ -61,6 +61,7 @@ import (
 	_ "flashcat.cloud/categraf/inputs/sqlserver"
 	_ "flashcat.cloud/categraf/inputs/switch_legacy"
 	_ "flashcat.cloud/categraf/inputs/system"
+	_ "flashcat.cloud/categraf/inputs/systemd"
 	_ "flashcat.cloud/categraf/inputs/tomcat"
 	_ "flashcat.cloud/categraf/inputs/vsphere"
 	_ "flashcat.cloud/categraf/inputs/zookeeper"
