2f2cb2ad3fceccd570ac5cf931b03e8fdf902dad
kongfei605 <kongfei605@gmail.com>
2023-05-08 19:14:54 +0800

    delete redundant snakecase (#498)

--- inputs/aliyun/internal/manager/manager.go
+++ inputs/aliyun/internal/manager/manager.go
@@ -1,8 +1,6 @@
 package manager
 
 import (
-	"unicode"
-
 	cms20190101 "github.com/alibabacloud-go/cms-20190101/v8/client"
 	cms2021101 "github.com/alibabacloud-go/cms-export-20211101/v2/client"
 )
@@ -46,24 +44,4 @@ func New(opts ...Option) (*Manager, error) {
 		}
 	}
 	return m, nil
-}
-
-func SnakeCase(in string) string {
-	runes := []rune(in)
-	length := len(runes)
-
-	var out []rune
-	for i := 0; i < length; i++ {
-		if runes[i] == '.' {
-			continue
-		}
-		if i > 0 && unicode.IsUpper(runes[i]) && ((i+1 < length && unicode.IsLower(runes[i+1])) || unicode.IsLower(runes[i-1])) {
-			if runes[i-1] != '_' {
-				out = append(out, '_')
-			}
-		}
-		out = append(out, unicode.ToLower(runes[i]))
-	}
-
-	return string(out)
 }
