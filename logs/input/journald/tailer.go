4ce1270b13f6a0283636d58ac124632285ec8b03
kongfei <kongfei605@gmail.com>
2023-08-10 20:37:58 +0800

    revert jorunald

--- logs/input/journald/tailer.go
+++ logs/input/journald/tailer.go
@@ -1,4 +1,4 @@
-//go:build !no_logs && linux && amd64
+//go:build !no_logs && systemd
 
 // Unless explicitly stated otherwise all files in this repository are licensed
 // under the Apache License Version 2.0.
