15cf97d390bebb5f3f6e6c02c7c8c3221f7894d0
kongfei <kongfei605@gmail.com>
2023-10-18 20:39:29 +0800

    osx service for categraf

---
+++ agent/install/service_darwin.go
@@ -0,0 +1,24 @@
+package install
+
+import (
+	"github.com/kardianos/service"
+)
+
+const (
+	ServiceName = "categraf"
+)
+
+var (
+	serviceConfig = &service.Config{
+		// 服务显示名称
+		Name: ServiceName,
+		// 服务名称
+		DisplayName: "categraf",
+		// 服务描述
+		Description: "Opensource telemetry collector",
+	}
+)
+
+func ServiceConfig() *service.Config {
+	return serviceConfig
+}
