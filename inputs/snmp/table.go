7e35ab12b574d05779d2fb7375b9de7b8a7d21e7
xtan <286809395@qq.com>
2023-12-08 19:50:24 +0800

    feat: extracting flow values from the rune array

--- inputs/snmp/table.go
+++ inputs/snmp/table.go
@@ -12,6 +12,7 @@ import (
 	"strings"
 	"time"
 
+	"flashcat.cloud/categraf/config"
 	"github.com/Knetic/govaluate"
 	"github.com/gosnmp/gosnmp"
 )
@@ -548,6 +549,10 @@ func fieldConvert(conv string, v interface{}) (interface{}, error) {
 		return v, nil
 	}
 
+	if config.Config.DebugMode {
+		log.Printf("D! %s the value %v", conv, v)
+	}
+
 	var d int
 	if _, err := fmt.Sscanf(conv, "float(%d)", &d); err == nil || conv == "float" {
 		switch vt := v.(type) {
@@ -691,6 +696,61 @@ func fieldConvert(conv string, v interface{}) (interface{}, error) {
 		}
 
 		return v, nil
+	}
+
+	// 55 57 57 51 46 56 32 77 66   may be the ascii rune arr for string ->  7993.8 MB
+	if conv == "asciitobytes" {
+
+		input, ok := v.([]uint8)
+		if !ok {
+			return nil, fmt.Errorf("invalid type of %v (not rune arr)", v)
+		}
+
+		// 将 []uint8 转换为字符切片
+		chars := make([]rune, len(input))
+		for i, val := range input {
+			chars[i] = rune(val)
+		}
+		// 将字符切片转换为字符串
+		asciiStr := string(chars)
+
+		// 提取数值
+		var numericStr string
+		for _, char := range asciiStr {
+			if char >= '0' && char <= '9' || char == '.' {
+				numericStr += string(char)
+			}
+		}
+
+		// 将字符串转换为浮点数
+		value, err := strconv.ParseFloat(numericStr, 64)
+		if err != nil {
+			return nil, fmt.Errorf("invalid number part of %s", asciiStr)
+		}
+
+		// 解析单位并转换为字节数
+		unit := strings.ToUpper(strings.TrimSpace(strings.Trim(asciiStr, numericStr)))
+		var result float64
+		switch unit {
+		case "":
+			result = value
+		case "B":
+			result = value
+		case "KB":
+			result = value * 1024
+		case "MB":
+			result = value * 1024 * 1024
+		case "GB":
+			result = value * 1024 * 1024 * 1024
+		case "TB":
+			result = value * 1024 * 1024 * 1024 * 1024
+		case "PB":
+			result = value * 1024 * 1024 * 1024 * 1024 * 1024
+		default:
+			return nil, fmt.Errorf("invalid unit of %s", unit)
+		}
+
+		return result, nil
 	}
 
 	return nil, fmt.Errorf("invalid conversion type '%s'", conv)
