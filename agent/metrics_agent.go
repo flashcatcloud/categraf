856f50d128e94576224450ef494a78c27270ae02
mt <375477717@qq.com>
2024-02-02 19:59:57 +0800

    Update metrics_agent.go

--- agent/metrics_agent.go
+++ agent/metrics_agent.go
@@ -83,6 +83,7 @@ import (
 	_ "flashcat.cloud/categraf/inputs/systemd"
 	_ "flashcat.cloud/categraf/inputs/tengine"
 	_ "flashcat.cloud/categraf/inputs/tomcat"
+	_ "flashcat.cloud/categraf/inputs/traffic_server"
 	_ "flashcat.cloud/categraf/inputs/vsphere"
 	_ "flashcat.cloud/categraf/inputs/whois"
 	_ "flashcat.cloud/categraf/inputs/xskyapi"
