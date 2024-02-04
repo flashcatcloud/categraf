32a56e0f7a0e2b1812fa5b90d103d39272fa8bd5
kongfei <kongfei605@gmail.com>
2024-02-04 08:00:39 +0800

    add node_exporter plugin

---
+++ inputs/node_exporter/collector/meminfo.go
@@ -0,0 +1,68 @@
+// Copyright 2015 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build (darwin || linux || openbsd || netbsd) && !nomeminfo
+// +build darwin linux openbsd netbsd
+// +build !nomeminfo
+
+package collector
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+const (
+	memInfoSubsystem = "memory"
+)
+
+type meminfoCollector struct {
+}
+
+func init() {
+	registerCollector("meminfo", defaultDisabled, NewMeminfoCollector)
+}
+
+// NewMeminfoCollector returns a new Collector exposing memory stats.
+func NewMeminfoCollector() (Collector, error) {
+	return &meminfoCollector{}, nil
+}
+
+// Update calls (*meminfoCollector).getMemInfo to get the platform specific
+// memory metrics.
+func (c *meminfoCollector) Update(ch chan<- prometheus.Metric) error {
+	var metricType prometheus.ValueType
+	memInfo, err := c.getMemInfo()
+	if err != nil {
+		return fmt.Errorf("couldn't get meminfo: %w", err)
+	}
+	// level.Debug(c.logger).Log("msg", "Set node_mem", "memInfo", memInfo)
+	for k, v := range memInfo {
+		if strings.HasSuffix(k, "_total") {
+			metricType = prometheus.CounterValue
+		} else {
+			metricType = prometheus.GaugeValue
+		}
+		ch <- prometheus.MustNewConstMetric(
+			prometheus.NewDesc(
+				prometheus.BuildFQName(namespace, memInfoSubsystem, k),
+				fmt.Sprintf("Memory information field %s.", k),
+				nil, nil,
+			),
+			metricType, v,
+		)
+	}
+	return nil
+}
---
+++ inputs/node_exporter/exporter.go
@@ -0,0 +1,79 @@
+package node_exporter
+
+import (
+	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"log"
+
+	"flashcat.cloud/categraf/config"
+	"flashcat.cloud/categraf/inputs"
+	"flashcat.cloud/categraf/inputs/node_exporter/collector"
+	"flashcat.cloud/categraf/types"
+)
+
+const (
+	inputName = "node_exporter"
+)
+
+type (
+	Exporter struct {
+		Typ string `toml:"type"`
+		config.PluginConfig
+
+		Collectors []string `toml:"collectors"`
+
+		exporterMetricsRegistry *prometheus.Registry
+		nc                      *collector.NodeCollector
+	}
+)
+
+var _ inputs.Input = new(Exporter)
+var _ inputs.SampleGatherer = new(Exporter)
+
+func init() {
+	inputs.Add(inputName, func() inputs.Input {
+		return &Exporter{}
+	})
+}
+
+func (e *Exporter) Init() error {
+	if len(e.Collectors) == 0 {
+		return types.ErrInstancesEmpty
+	}
+
+	r := prometheus.NewRegistry()
+	e.exporterMetricsRegistry = r
+	nc, err := collector.NewNodeCollector(e.Collectors...)
+	if err != nil {
+		return fmt.Errorf("couldn't create collector: %s", err)
+	}
+	e.nc = nc
+
+	if err := e.exporterMetricsRegistry.Register(e.nc); err != nil {
+		return fmt.Errorf("couldn't register node collector: %s", err)
+	}
+	return nil
+}
+
+func (e *Exporter) Clone() inputs.Input {
+	return &Exporter{}
+}
+
+func (e *Exporter) Name() string {
+	return inputName
+}
+
+func (e *Exporter) Type() string {
+	return e.Typ
+}
+
+func (e *Exporter) Drop() {
+}
+
+func (e *Exporter) Gather(slist *types.SampleList) {
+	labels := e.GetLabels()
+	err := inputs.Collect(e.nc, slist, labels)
+	if err != nil {
+		log.Println("E! node exporter collects error:", err)
+	}
+}
