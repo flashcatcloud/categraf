6ac47e604410dbd934e0d0b9aea1b7299e0d51ca
kongfei <kongfei605@gmail.com>
2024-03-25 20:03:58 +0800

    refactor: kafka metrics for logs agent

--- logs/client/kafka/producer.go
+++ logs/client/kafka/producer.go
@@ -1,19 +1,24 @@
 package kafka
 
 import (
-	"flashcat.cloud/categraf/types"
-	"flashcat.cloud/categraf/writer"
 	"fmt"
 	"log"
-	"sync"
 	"time"
 
 	"github.com/IBM/sarama"
+
+	"flashcat.cloud/categraf/logs/util"
+	"flashcat.cloud/categraf/types"
+	"flashcat.cloud/categraf/writer"
 )
 
 const (
 	AsyncProducer = "async"
 	SyncProducer  = "sync"
+
+	Worker   = "categraf_logs_agent"
+	Mname    = "send_kafka_messages"
+	AllTopic = "categraf_all_topics"
 )
 
 type (
@@ -23,110 +28,18 @@ type (
 		Metrics()
 	}
 
-	metric struct {
-		sync.Mutex
-		success int64
-		failed  int64
-		sent    int64
-		counter map[string]*unit
-	}
-	unit struct {
-		success int64
-		failed  int64
-		sent    int64
-	}
-
 	AsyncProducerWrapper struct {
 		asyncProducer sarama.AsyncProducer
 		stop          chan struct{}
-		metric        *metric
+		slist         *types.SampleList
 	}
 
 	SyncProducerWrapper struct {
 		syncProducer sarama.SyncProducer
 		stop         chan struct{}
-		metric       *metric
+		slist        *types.SampleList
 	}
 )
-
-func (m *metric) inc(topic, typ string) {
-	m.Lock()
-	defer m.Unlock()
-	switch typ {
-	case "failed":
-		m.failed++
-	case "success":
-		m.success++
-	case "sent":
-		m.sent++
-	}
-
-	if len(topic) == 0 {
-		return
-	}
-
-	if m.counter[topic] == nil {
-		m.counter[topic] = &unit{
-			success: 0,
-			failed:  0,
-			sent:    0,
-		}
-	}
-	switch typ {
-	case "failed":
-		m.counter[topic].failed++
-	case "success":
-		m.counter[topic].success++
-	case "sent":
-		m.counter[topic].sent++
-	}
-}
-
-func (m *metric) Samples() []*types.Sample {
-	m.Lock()
-	defer m.Unlock()
-	const (
-		worker   = "categraf_logs_agent"
-		mName    = "send_kafka_messages"
-		allTopic = "categraf_all_topics"
-	)
-	now := time.Now()
-	samples := append([]*types.Sample{},
-		types.NewSample("", mName, m.sent, map[string]string{
-			"source": worker,
-			"type":   "sent",
-			"topic":  allTopic,
-		}).SetTime(now))
-	samples = append(samples, types.NewSample("", mName, m.success, map[string]string{
-		"source": worker,
-		"type":   "success",
-		"topic":  allTopic,
-	}).SetTime(now))
-	samples = append(samples, types.NewSample("", mName, m.failed, map[string]string{
-		"source": worker,
-		"type":   "failed",
-		"topic":  allTopic,
-	}).SetTime(now))
-	for k, v := range m.counter {
-		samples = append(samples, types.NewSample("", mName, v.sent, map[string]string{
-			"source": worker,
-			"type":   "sent",
-			"topic":  k,
-		}).SetTime(now))
-		samples = append(samples, types.NewSample("", mName, v.success, map[string]string{
-			"source": worker,
-			"type":   "success",
-			"topic":  k,
-		}).SetTime(now))
-		samples = append(samples, types.NewSample("", mName, v.failed, map[string]string{
-			"source": worker,
-			"type":   "failed",
-			"topic":  k,
-		}).SetTime(now))
-	}
-
-	return samples
-}
 
 func New(typ string, brokers []string, config *sarama.Config) (Producer, error) {
 	stop := make(chan struct{})
@@ -139,9 +52,7 @@ func New(typ string, brokers []string, config *sarama.Config) (Producer, error) 
 		apw := &AsyncProducerWrapper{
 			asyncProducer: p,
 			stop:          stop,
-			metric: &metric{
-				counter: make(map[string]*unit),
-			},
+			slist:         types.NewSampleList(),
 		}
 		go apw.errorWorker()
 		go apw.successWorker()
@@ -151,9 +62,7 @@ func New(typ string, brokers []string, config *sarama.Config) (Producer, error) 
 		return &SyncProducerWrapper{
 			syncProducer: p,
 			stop:         stop,
-			metric: &metric{
-				counter: make(map[string]*unit),
-			},
+			slist:        types.NewSampleList(),
 		}, err
 	default:
 		return nil, fmt.Errorf("unknown producer type: %s", typ)
@@ -161,7 +70,11 @@ func New(typ string, brokers []string, config *sarama.Config) (Producer, error) 
 }
 
 func (p *AsyncProducerWrapper) Send(msg *sarama.ProducerMessage) error {
-	go p.metric.inc("", "sent")
+	go p.slist.PushFront(types.NewSample("", Mname, 1, map[string]string{
+		"source": Worker,
+		"status": "sent",
+		"topic":  AllTopic,
+	}).SetTime(time.Now()))
 	p.asyncProducer.Input() <- msg
 	return nil
 }
@@ -174,9 +87,14 @@ func (p AsyncProducerWrapper) Metrics() {
 		case <-p.stop:
 			return
 		case <-timer.C:
-			samples := p.metric.Samples()
-			for _, s := range samples {
-				log.Println("DEBUG====", s.Metric, s.Value, s.Labels)
+			if p.slist == nil || p.slist.Len() == 0 {
+				continue
+			}
+			samples := p.slist.PopBackAll()
+			if util.Debug() {
+				for _, s := range samples {
+					log.Println("DEBUG====", s.Metric, s.Value, s.Labels, s.Timestamp)
+				}
 			}
 			writer.WriteSamples(samples)
 		}
@@ -192,8 +110,12 @@ func (p *AsyncProducerWrapper) errorWorker() {
 	for {
 		select {
 		case err := <-p.asyncProducer.Errors():
-			p.metric.inc("", "failed")
 			log.Println("E! kafka producer error", err)
+			p.slist.PushFront(types.NewSample("", Mname, 1, map[string]string{
+				"source": Worker,
+				"status": "failed",
+				"topic":  AllTopic,
+			}).SetTime(time.Now()))
 		case <-p.stop:
 			return
 		}
@@ -204,7 +126,11 @@ func (p *AsyncProducerWrapper) successWorker() {
 	for {
 		select {
 		case <-p.asyncProducer.Successes():
-			p.metric.inc("", "success")
+			p.slist.PushFront(types.NewSample("", Mname, 1, map[string]string{
+				"source": Worker,
+				"status": "success",
+				"topic":  AllTopic,
+			}).SetTime(time.Now()))
 		case <-p.stop:
 			return
 		}
@@ -213,11 +139,29 @@ func (p *AsyncProducerWrapper) successWorker() {
 
 func (p *SyncProducerWrapper) Send(msg *sarama.ProducerMessage) error {
 	_, _, err := p.syncProducer.SendMessage(msg)
-	go p.metric.inc(msg.Topic, "sent")
+	now := time.Now()
+	go p.slist.PushFront(types.NewSample("", Mname, 1, map[string]string{
+		"source": Worker,
+		"status": "sent",
+		"topic":  AllTopic,
+	}).SetTime(now))
+	go p.slist.PushFront(types.NewSample("", Mname, 1, map[string]string{
+		"source": Worker,
+		"status": "sent",
+		"topic":  msg.Topic,
+	}).SetTime(now))
 	if err == nil {
-		go p.metric.inc(msg.Topic, "success")
+		go p.slist.PushFront(types.NewSample("", Mname, 1, map[string]string{
+			"source": Worker,
+			"status": "success",
+			"topic":  AllTopic,
+		}).SetTime(now))
 	} else {
-		go p.metric.inc(msg.Topic, "failed")
+		go p.slist.PushFront(types.NewSample("", Mname, 1, map[string]string{
+			"source": Worker,
+			"status": "failed",
+			"topic":  AllTopic,
+		}).SetTime(now))
 	}
 	return err
 }
@@ -235,7 +179,11 @@ func (p *SyncProducerWrapper) Metrics() {
 		case <-p.stop:
 			return
 		case <-timer.C:
-			writer.WriteSamples(p.metric.Samples())
+			if p.slist == nil || p.slist.Len() == 0 {
+				continue
+			}
+			samples := p.slist.PopBackAll()
+			writer.WriteSamples(samples)
 		}
 	}
 }
