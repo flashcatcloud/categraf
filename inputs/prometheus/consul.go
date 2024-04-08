cf0956ae81322803be2b066b6dc3a4cdee374c11
xmh1011 <1197843839@qq.com>
2024-04-08 12:23:04 +0800

    feat: prometheus consul service auto discovery
    Signed-off-by: xmh1011 <1197843839@qq.com>

--- inputs/prometheus/consul.go
+++ inputs/prometheus/consul.go
@@ -2,11 +2,16 @@ package prometheus
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"log"
 	"net/url"
 	"strings"
+	"sync"
 	"text/template"
+	"time"
+
+	"flashcat.cloud/categraf/config"
 
 	"github.com/hashicorp/consul/api"
 )
@@ -14,10 +19,11 @@ import (
 type ConsulConfig struct {
 	// Address of the Consul agent. The address must contain a hostname or an IP address
 	// and optionally a port (format: "host:port").
-	Enabled bool           `toml:"enabled"`
-	Agent   string         `toml:"agent"`
-	Queries []*ConsulQuery `toml:"query"`
-	Catalog *api.Catalog   `toml:"-"`
+	Enabled       bool            `toml:"enabled"`
+	Agent         string          `toml:"agent"`
+	QueryInterval config.Duration `toml:"query_interval"`
+	Queries       []*ConsulQuery  `toml:"query"`
+	Catalog       *api.Catalog    `toml:"-"`
 }
 
 // One Consul service discovery query
@@ -40,6 +46,9 @@ type ConsulQuery struct {
 
 	serviceURLTemplate       *template.Template
 	serviceExtraTagsTemplate map[string]*template.Template
+
+	// Store last error status and change log level depending on repeated occurrence
+	lastQueryFailed bool
 }
 
 func (ins *Instance) InitConsulClient() error {
@@ -85,7 +94,7 @@ func (ins *Instance) InitConsulClient() error {
 	return nil
 }
 
-func (ins *Instance) UrlsFromConsul() ([]ScrapeUrl, error) {
+func (ins *Instance) UrlsFromConsul(ctx context.Context) ([]ScrapeUrl, error) {
 	if !ins.ConsulConfig.Enabled {
 		return []ScrapeUrl{}, nil
 	}
@@ -135,7 +144,113 @@ func (ins *Instance) UrlsFromConsul() ([]ScrapeUrl, error) {
 		}
 	}
 
+	if ins.firstRun {
+		var wg sync.WaitGroup
+		consulAPIConfig := api.DefaultConfig()
+		if ins.ConsulConfig.Agent != "" {
+			consulAPIConfig.Address = ins.ConsulConfig.Agent
+		}
+
+		consul, err := api.NewClient(consulAPIConfig)
+		if err != nil {
+			return []ScrapeUrl{}, fmt.Errorf("cannot connect to the Consul agent: %w", err)
+		}
+		catalog := consul.Catalog()
+
+		wg.Add(1)
+		go func() {
+			// Store last error status and change log level depending on repeated occurrence
+			var refreshFailed = false
+			defer wg.Done()
+			err := ins.refreshConsulServices(catalog)
+			if err != nil {
+				refreshFailed = true
+				log.Printf("Unable to refresh Consul services: %v\n", err)
+			}
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case <-time.After(time.Duration(ins.ConsulConfig.QueryInterval)):
+					err := ins.refreshConsulServices(catalog)
+					if err != nil {
+						message := fmt.Sprintf("Unable to refresh Consul services: %v", err)
+						if refreshFailed {
+							log.Println("E!", message)
+						} else {
+							log.Println("W!", message)
+						}
+						refreshFailed = true
+					} else if refreshFailed {
+						refreshFailed = false
+						log.Println("Successfully refreshed Consul services after previous errors")
+					}
+				}
+			}
+		}()
+		ins.firstRun = false
+		wg.Wait()
+	}
+
 	return returls, nil
+}
+
+func (ins *Instance) refreshConsulServices(c *api.Catalog) error {
+	consulServiceURLs := make(map[string]ScrapeUrl)
+
+	if ins.DebugMod {
+		log.Println("Refreshing Consul services")
+	}
+
+	for _, q := range ins.ConsulConfig.Queries {
+		queryOptions := api.QueryOptions{}
+		if q.ServiceDc != "" {
+			queryOptions.Datacenter = q.ServiceDc
+		}
+
+		// Request services from Consul
+		consulServices, _, err := c.Service(q.ServiceName, q.ServiceTag, &queryOptions)
+		if err != nil {
+			return err
+		}
+		if len(consulServices) == 0 {
+			if ins.DebugMod {
+				log.Printf("Queried Consul for Service (%s, %s) but did not find any instances\n", q.ServiceName, q.ServiceTag)
+			}
+			continue
+		}
+		if ins.DebugMod {
+			log.Printf("Queried Consul for Service (%s, %s) and found %d instances\n", q.ServiceName, q.ServiceTag, len(consulServices))
+		}
+
+		for _, consulService := range consulServices {
+			uaa, err := ins.getConsulServiceURL(q, consulService)
+			if err != nil {
+				message := fmt.Sprintf("Unable to get scrape URLs from Consul for Service (%s, %s): %s", q.ServiceName, q.ServiceTag, err)
+				if q.lastQueryFailed {
+					log.Println("E!", message)
+				} else {
+					log.Println("W!", message)
+				}
+				q.lastQueryFailed = true
+				break
+			}
+			if q.lastQueryFailed {
+				log.Printf("Created scrape URLs from Consul for Service (%s, %s)\n", q.ServiceName, q.ServiceTag)
+			}
+			q.lastQueryFailed = false
+			log.Printf("Adding scrape URL from Consul for Service (%s, %s): %s\n", q.ServiceName, q.ServiceTag, uaa.URL.String())
+			consulServiceURLs[uaa.URL.String()] = *uaa
+		}
+	}
+
+	ins.lock.Lock()
+	for _, u := range consulServiceURLs {
+		ins.URLs = append(ins.URLs, u.URL.String())
+	}
+	ins.lock.Unlock()
+
+	return nil
 }
 
 func (ins *Instance) getConsulServiceURL(q *ConsulQuery, s *api.CatalogService) (*ScrapeUrl, error) {
